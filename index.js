9f91c0225e71f7e0e1775753378d1e65e2c36de1
LiamMcAliffe <lmmcauliffe1223@gmail.com>
2025-01-27 21:27:37 -0500

    Added UI to display score, action, and buttons to chose action for the user.

--- index.js
+++ index.js
@@ -8,80 +8,96 @@ let scissors = "scissors";
 
 let options = [rock, paper, scissors];
 
-let humanScore = 0;
+let userScore = 0;
 let computerScore = 0;
 
 /* Computer Chooses an Option */
 function getComputerChoice() {
-  randNum = Math.floor(Math.random() * 3);
-  return options[randNum];
+    randNum = Math.floor(Math.random() * 3);
+    return options[randNum];
 }
 
-/* Human Chooses an Option */
-function getHumanChoice() {
-  userChoice = window.prompt("rock, paper, or scissors");
-  return userChoice;
+function playRound(userChoice, computerChoice) {
+    console.log(`user chose: ${userChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    updateAction(userChoice, computerChoice);
+    switch (userChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("Its a Tie!");
+                    break;
+                case "paper":
+                    console.log("Computer Wins!");
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log("user Wins!");
+                    userScore++;
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("user Wins!");
+                    userScore++;
+                    break;
+                case "paper":
+                    console.log("Its a Tie!");
+                    break;
+                case "scissors":
+                    console.log("Computer Wins!");
+                    computerScore++;
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("Computer Wins!");
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log("user Wins!");
+                    userScore++;
+                    break;
+                case "scissors":
+                    console.log("Its a Tie!");
+                    break;
+            }
+            break;
+    }
+    console.log(`user Score: ${userScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+    updateScore();
 }
 
-function playRound(humanChoice, computerChoice) {
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
-  humanChoice = humanChoice.toLowerCase();
-  switch (humanChoice) {
-    case "rock":
-      switch (computerChoice) {
-        case "rock":
-          console.log("Its a Tie!");
-          break;
-        case "paper":
-          console.log("Computer Wins!");
-          computerScore++;
-          break;
-        case "scissors":
-          console.log("Human Wins!");
-          humanScore++;
-          break;
-      }
-      break;
-    case "paper":
-      switch (computerChoice) {
-        case "rock":
-          console.log("Human Wins!");
-          humanScore++;
-          break;
-        case "paper":
-          console.log("Its a Tie!");
-          break;
-        case "scissors":
-          console.log("Computer Wins!");
-          computerScore++;
-          break;
-      }
-      break;
-    case "scissors":
-      switch (computerChoice) {
-        case "rock":
-          console.log("Computer Wins!");
-          computerScore++;
-          break;
-        case "paper":
-          console.log("Human Wins!");
-          humanScore++;
-          break;
-        case "scissors":
-          console.log("Its a Tie!");
-          break;
-      }
-      break;
-  }
-  console.log(`Human Score: ${humanScore}`);
-  console.log(`Computer Score: ${computerScore}`);
+function updateScore() {
+    document.getElementById("computer-score").textContent = computerScore;
+    document.getElementById("user-score").textContent = userScore;
 }
 
-getHumanChoice();
-playRound(userChoice, getComputerChoice());
+function updateAction(userChoice, computerChoice) {
+    document.getElementById(
+        "computer-action"
+    ).textContent = `${computerChoice.toUpperCase()}!`;
+    document.getElementById(
+        "user-action"
+    ).textContent = `${userChoice.toUpperCase()}!`;
+}
 
-let button = document.getElementById("play-again");
-button.addEventListener("click", () => {
-  playRound(getHumanChoice(), getComputerChoice());
+let rockBtn = document.getElementById("rock");
+rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+
+let paperBtn = document.getElementById("paper");
+paperBtn.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+});
+
+let scissorsBtn = document.getElementById("scissors");
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
 });
